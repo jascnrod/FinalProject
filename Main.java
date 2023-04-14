1554d05ee23cbac5ff636675235fec2946b43b92
Jason <100395902+jascnrod@users.noreply.github.com>
2023-04-13 20:04:30 -0400

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@
 //ok so we can make changes on github
 public class Main {
     public static void main ( String []  args) {
-
+        System.out.println("I just goave myself a head ache");
     }
 }
