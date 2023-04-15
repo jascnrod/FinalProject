296272e3343e6fe97403ebef43bfb5b36c937894
Maria <xranmq@knights.ucf.edu>
2023-04-15 18:06:35 -0400

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 // what up foolz
 //uh oh stinky
+//hellos
 
 // jasdjfjjdsfjajasdfj
 public class Main {
@@ -254,7 +255,7 @@ class List {
             if (e.getUcfid() != id) {    
                 return true;
             }
-            }
+        }
 
         return false;
     }
