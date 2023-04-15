9950b6b68825822c0086ec13f62e9385ad3c4bd0
Jason <100395902+jascnrod@users.noreply.github.com>
2023-04-15 13:19:20 -0400

    modified:   Main.java

--- Main.java
+++ Main.java
@@ -5,26 +5,24 @@ public class Main {
     public static void main(String[] args) {
         int x = 0, INPUTMAX = 5;
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Knight> list = new ArrayList<Knight>();// Array list adds objects to it infinitely no need to
         while (x != 7) {
+            int count = 0;// goes through input array
+            String[] userInputStr = new String[INPUTMAX];
             System.out.println(
-                    "1: Add a new faculty to the schedule.\n" +
-                            "2: Enroll a student to a lecture (and to of of its labs if applicable)\n" +
-                            "3: Print the Schedule of a faculty.\n" +
-                            "4: Print the schedule of an TA.\n" +
-                            "5: Print the schedule of a student.\n" +
-                            "6: Delete a scheduled lecture\n" +
-                            "7: Exit Program");
-            System.out.println("Type the number for what you want to do.");
-            x = scanner.nextInt();
+                "1: Add a new faculty to the schedule.\n" +
+                "2: Enroll a student to a lecture (and to of of its labs if applicable)\n" +
+                "3: Print the Schedule of a faculty.\n" +
+                "4: Print the schedule of an TA.\n" +
+                "5: Print the schedule of a student.\n" +
+                "6: Delete a scheduled lecture\n" +
+                "7: Exit Program");
+                System.out.println("Type the number for what you want to do.");
+                x = scanner.nextInt();
             
-            //There needs to be exception handling the entry of the wrong numbers
-            
-            ArrayList<Knight> list = new ArrayList<Knight>();// Array list adds objects to it infinitely no need to
-                                                             // specify length
+            //There needs to be exception handling the entry of the wrong numbers 
             switch (x) {
                 case 1:
-                    int count = 0;// goes through input array
-                    String[] userInputStr = new String[INPUTMAX];
                     System.out.print("Enter UCF id:");
                     userInputStr[count] = scanner.nextLine();
                     count++;
@@ -51,6 +49,11 @@ public class Main {
                     
                     break;
                 case 2:
+                    count = 0;// goes through input array
+                    userInputStr = new String[INPUTMAX];
+                    System.out.print("Enter UCF id:");
+                    userInputStr[count] = scanner.nextLine();
+                    count++;
                     break;
                 case 3:
                     break;
